f26f350023c2a4f62c01975e296584238b034586
kevin <kl.klee.k.lee@gmail.com>
2023-08-10 14:48:42 -0400

    class in OOP Exercise

---
+++ ch17OOPs/17.1.12.js
@@ -0,0 +1,61 @@
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return 'Beep.';
+    }
+    toString() {
+        return `This vehicle is a ${this.make} ${this.model} from ${this.year}.`
+    }
+
+}
+
+
+
+class Vehicle extends Car {
+    constructor(make, model, year) {
+        super(make, model, year)
+        this.numWheels = 4;
+
+    }
+
+
+}
+
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year)
+        this.numWheels = 2;
+    }
+    revEngine() {
+        return "VROOM!";
+    }
+}
+
+
+//GARAGE
+
+class Garage {
+    constructor(a) {
+
+
+        this.capacity = a;
+        this.vehicles = [];
+
+
+    }
+    add(newVehicle) {
+        if (!(newVehicle instanceof Car)) {
+            return 'Only vehicles are allowed in here!';
+        }
+        if (this.vehicles.length >= this.capacity) {
+            return 'Sorry, we\'re full';
+        }
+        this.vehicles.push(newVehicle);
+        return 'Vehicle added!';
+    }
+}
