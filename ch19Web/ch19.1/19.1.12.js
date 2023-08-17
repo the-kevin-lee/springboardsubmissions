3af8411b355df71f4eb28179dbf4ba21c2d6b749
kevin <kl.klee.k.lee@gmail.com>
2023-08-17 07:07:44 -0400

    How the Web Works Exercise

---
+++ ch19Web/ch19.1/19.1.12.js
@@ -0,0 +1,28 @@
+// 1)   An HTTP is a type of web protocol (Hypertext Transfer Protocol) that regulates how and under which set of rules a particular request should be sent and how a response should be sent back
+
+// 2)   A URL (uniform resource locator) is a unique identifier (like an address) that will locate a particular webpage over the internet. It is composed of a web protocol, typically HTTP/HTTPS/or XML, a Host Name 
+// that transfers into an IP address,  a port number (version under which the IP address will be sent), a resource (unique set of characters that determines a specific webpage within a website), and a query (set of strings)
+// that logs and queries certain search terms.)
+
+// 3) A DNS (Domain Name System) is a specific type of web server that works alongside other DNS servers to retrieve a specific IP address in their records when a client requests a webpage. It will send
+// back that IP address to the client for the client to then locate a particular website and request the webpage from the request.
+
+// 4) A query string is a series of letters, numbers, and symbols that are arranged towards the end of the URL that specify a certain set of filters, search parameters, page number, or other type of parameter
+// that will then be sent over to the server to then parse through and send a specific response back according to the parameters provided. In this way the response will be unique. 
+
+// 5) The get method allows the client to retrieve data or fetch documents from the server.
+// The post method allows the client to submit data (especially sensitive data) over the internet to the server in order to create
+//or update a new resource within the server. 
+
+// 6) An HTTP request allows the user to invoke a request over a server or API to retrieve certain data for clientside or to post certain data to that particular
+// server or API.
+
+// 7) An HTTP response is what the client receives over the network from the server or API
+
+// 8) An HTTP header provides additional details in regard to the request/response. Some examples are: Host, User-Agent, Cache-Control
+
+// 9)  Your request over a URL will allow the browser to change the host name into an IP address that is provided from the DNS. It will then send a request to 
+// that particular IP address along with any headers.
+// The server or API comes back with a response that the browser will then translate into the DOM to become visible content for th user-end experience.    
+
+
