931fa9980a16ad4ae475bb41a32f89bd37162950
kevin <kl.klee.k.lee@gmail.com>
2023-08-21 09:41:53 -0400

    Jasmine

--- ch12JasmineTesting/12.12/Server Tip Calculator/servers.js
+++ ch12JasmineTesting/12.12/Server Tip Calculator/servers.js
@@ -1,44 +1,48 @@
+// Getting DOM elements for server name input, server form, and server table body
 let serverNameInput = document.getElementById('serverName');
 let serverForm = document.getElementById('serverForm');
-
 let serverTbody = document.querySelector('#serverTable tbody');
 
+// Object to hold all servers and a variable for server ID
 let allServers = {};
 let serverId = 0;
 
+// Adding an event listener to the server form for the submit event
 serverForm.addEventListener('submit', submitServerInfo);
 
-// create server object and add to allServers, update html and reset input
+// Function to create a server object, add it to allServers, update the HTML table, and reset the input
 function submitServerInfo(evt) {
-  if (evt) evt.preventDefault(); // when running tests there is no event
+  if (evt) evt.preventDefault(); // Preventing the default form submission action, especially when running tests
 
-  let serverName = serverNameInput.value;
+  let serverName = serverNameInput.value; // Getting the value of the server name input
 
+  // If serverName is not empty, create a new server entry
   if (serverName !== '') {
-    serverId++;
-    allServers['server' + serverId] = { serverName };
+    serverId++; // Increment server ID
+    allServers['server' + serverId] = { serverName }; // Add new server object to allServers
 
-    updateServerTable();
+    updateServerTable(); // Update the server table in the HTML
 
-    serverNameInput.value = '';
+    serverNameInput.value = ''; // Reset the server name input
   }
 }
 
-// Create table row element and pass to appendTd function with input value
+// Function to update the server table in the HTML
 function updateServerTable() {
-  serverTbody.innerHTML = '';
+  serverTbody.innerHTML = ''; // Clearing the table body
 
+  // Looping through all servers in allServers object
   for (let key in allServers) {
-    let curServer = allServers[key];
+    let curServer = allServers[key]; // Current server object
 
-    let newTr = document.createElement('tr');
-    newTr.setAttribute('id', key);
+    let newTr = document.createElement('tr'); // Creating a new table row element
+    newTr.setAttribute('id', key); // Setting the ID attribute of the table row
 
-    let tipAverage = sumPaymentTotal('tipAmt') / Object.keys(allServers).length;
+    let tipAverage = sumPaymentTotal('tipAmt') / Object.keys(allServers).length; // Calculating the average tip
 
-    appendTd(newTr, curServer.serverName);
-    appendTd(newTr, '$' + tipAverage.toFixed(2));
+    appendTd(newTr, curServer.serverName); // Appending the server name to the table row
+    appendTd(newTr, '$' + tipAverage.toFixed(2)); // Appending the average tip to the table row
 
-    serverTbody.append(newTr);
+    serverTbody.append(newTr); // Appending the new table row to the table body
   }
 }
--- ch12JasmineTesting/12.12/Server Tip Calculator/servers.test.js
+++ ch12JasmineTesting/12.12/Server Tip Calculator/servers.test.js
@@ -1,25 +1,47 @@
+// Test suite for server-related functionality
 describe("Servers test (with setup and tear-down)", function() {
+  // Runs before each individual test within this describe block
   beforeEach(function () {
-    // initialization logic
+    // Initialization logic: setting the value of serverNameInput to 'Alice'
     serverNameInput.value = 'Alice';
   });
 
+  // Individual test case
   it('should add a new server to allServers on submitServerInfo()', function () {
+    // Calling the function to be tested
     submitServerInfo();
 
+    // Expectation: Checking that there is exactly one server in allServers
     expect(Object.keys(allServers).length).toEqual(1);
-    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    // Expectation: Checking that the server's name is 'Alice'
+    expect(allServers['server' + serverId].serverName).toEqual('');
   });
 
+  it('should not add a new server with an empty string' function() {
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(Object.keys(allServers).length.toEqaul(0));
+  })
+
+  it('should update #servertable upon updateServerTable()' function() {
+    submitServerInfo();
+    updateServerTable();
+
+    let TdList = document.querySelectorAll('#serverTable tbody tr td');
+
+expect(TdList.length).toEqaul(3);
+expect(TdList[0].innerText).toEqaul('Alice');
+expect(TdList[1].innerText).toEqaul('$0.00');
+
+
+  })
+
+  // Runs after each individual test within this describe block
   afterEach(function() {
-    // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
+
   });
 });
 
-describe("tip server functionality", function() {
-  
-  
-
-
-  
-});
